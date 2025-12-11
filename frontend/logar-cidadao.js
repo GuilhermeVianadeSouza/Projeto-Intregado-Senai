50118361d941635d84f49946a9d17c5be773cb9b
Victorhsilva1 <victor.hsilva2703@gmail.com>
2025-12-11 11:01:47 -0300

    iniciando btn classificacao

--- frontend/logar-cidadao.js
+++ frontend/logar-cidadao.js
@@ -1,6 +1,8 @@
 'use strict'
 
 export async function obterIdCidadao(email, senha) {
+
+    admin
     const url = `http://localhost:8080/v1/cidadao/email?email=${email}&senha=${senha}`
     const response = await fetch(url)
     const data = response.json()
