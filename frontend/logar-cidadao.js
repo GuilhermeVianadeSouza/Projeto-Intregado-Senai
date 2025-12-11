96e04f89d99ad2bdf072fefd327e396ce99c156e
Victorhsilva1 <victor.hsilva2703@gmail.com>
2025-12-11 11:04:08 -0300

    adicionando usuario padrao

--- frontend/logar-cidadao.js
+++ frontend/logar-cidadao.js
@@ -1,8 +1,12 @@
 'use strict'
 
 export async function obterIdCidadao(email, senha) {
-
-    admin
+    if (email === 'admin@admin.com' && senha === 'admin') {
+        return {
+            status_code: 200,
+            cidadao: [{ id: 1 }]
+        };
+    }
     const url = `http://localhost:8080/v1/cidadao/email?email=${email}&senha=${senha}`
     const response = await fetch(url)
     const data = response.json()
