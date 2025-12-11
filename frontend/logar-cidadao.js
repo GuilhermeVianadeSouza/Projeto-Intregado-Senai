92243236e059be1c01350fe48a87789870beb302
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-11 16:34:52 -0300

    feat: upload de fotos na azure

--- frontend/logar-cidadao.js
+++ frontend/logar-cidadao.js
@@ -7,7 +7,7 @@ export async function obterIdCidadao(email, senha) {
             cidadao: [{ id: 1 }]
         };
     }
-    const url = `http://localhost:8080/v1/cidadao/email?email=${email}&senha=${senha}`
+    const url = `http://localhost:8080/v1/cidadao?email=${email}&senha=${senha}`
     const response = await fetch(url)
     const data = response.json()
     return data
